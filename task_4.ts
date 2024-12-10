519ba831486496e218eaf1412319445715a07067
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2024-12-10 11:05:52 +0300

    feat: done task_4

---
+++ task_4.ts
@@ -0,0 +1,8 @@
+//Функция принимает 2 массива, и возвращает массив объединенных значений,
+//без дублирования
+
+function union(arr1: number[], arr2: number[]): number[] {
+    return Array.from(new Set([...arr1, ...arr2]));
+}
+
+console.log(union([5, 1, 2, 3, 3], [4, 3, 2]));
