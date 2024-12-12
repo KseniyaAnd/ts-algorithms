177f84de1506ac61d6ee7892f1dd3ae188a4b175
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2024-12-12 15:28:32 +0300

    fix: in task_1 use map instead forEach

--- task_3.ts
+++ task_3.ts
@@ -2,7 +2,9 @@
 //Первый аргумент - массив, второй - функция, применяется на массив в обратном порядке
 
 function forEachRight<T, R = void>(arr: T[], callback: (value: T) => R): R[] | void {
-    return arr.reverse().forEach(el => callback(el));
+    return arr.reverse().map(callback);
 }
 
-forEachRight<number | string>([1, 2, 3, 4], val => console.log(val));
+//forEachRight([1, 2, 3, 4], val => console.log(val));
+
+// console.log(forEachRight<number | string>([1, 2, 3, 4], (x) => x * 2));
