87dd17f563b2a2c69c640fbdcf303aec78aeebee
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2024-12-12 16:14:20 +0300

    fix: use Symbol.toPrimitive

--- task_9.ts
+++ task_9.ts
@@ -12,17 +12,25 @@ const obj = {
     id: 0,
     name: 'Obj-name',
 
-    toString() {
-        console.log('tostring')
-        return this.name
-    },
-
-    valueOf() {
-        console.log('valueof')
-        return 0;
+    [Symbol.toPrimitive](hint) {
+        return hint == "string" ? this.name : this.id;
     }
+
+    // toString() {
+    //     console.log('tostring')
+    //     return this.name
+    // },
+    //
+    // valueOf() {
+    //     console.log('valueof')
+    //     return 0;
+    // }
+
+    // [Symbol.toPrimitive]
 };
 
 console.log(`Name: ${obj}`); 		// Name: Obj-name
 console.log(+obj);            		// 0
 console.log(obj + 10);        		// 10
+
+
