55bcb28544ff625c8b6152a5714bcbc02151f33b
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2024-12-13 13:58:34 +0300

    init: done task_15 using recursive method

--- task_15.ts
+++ task_15.ts
@@ -1,8 +1,11 @@
 // Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
 
 function sumTo(n: number): number {
+
     //арефметическая прогрессия
-    return n * (n + 1) / 2;
+    //return n * (n + 1) / 2;
+
+    return n<=1 ? n : sumTo(n)
 }
 
 console.log(sumTo(5));
