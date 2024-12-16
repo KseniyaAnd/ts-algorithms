4cfb4a51f5691a9259156a98b36f5141d55f63c9
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2024-12-16 14:40:31 +0300

    fix: add types

--- task_17.ts
+++ task_17.ts
@@ -3,7 +3,7 @@
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 function filter_list<T>(arr: T[]): T[] {
-    return arr.filter(el => typeof el == "number")
+    return arr.filter(el =>  !isNaN(Number(el)))
 }
 
-console.log(filter_list([1,2,'a','b']));
+console.log(filter_list<number | string>(['1', 2,'a','b']));
